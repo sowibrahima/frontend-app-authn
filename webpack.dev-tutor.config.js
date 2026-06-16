14007c8263cf1f7e34676d6e18382cce74ed0baa
Ibrahima Sow <ibropers@gmail.com>
2026-06-16 15:41:15 +0200

    Align WutiSkill webpack theme contract

--- webpack.dev-tutor.config.js
+++ webpack.dev-tutor.config.js
@@ -1,55 +1,10 @@
 const { createConfig } = require('@openedx/frontend-build');
-const ParagonWebpackPlugin = require('@openedx/frontend-build/lib/plugins/paragon-webpack-plugin/ParagonWebpackPlugin');
-const webpack = require('webpack');
+const stripWutiskillParagonTheme = require('../webpack.wutiskill-theme');
 
 const config = createConfig('webpack-dev');
 
-// ---------------------------------------------------------------------------
-// Strip Paragon CSS from the build (tutor dev mode).
-// ---------------------------------------------------------------------------
+stripWutiskillParagonTheme(config);
 
-// 1. Remove Paragon entrypoints
-Object.keys(config.entry).forEach((key) => {
-    if (key.startsWith('paragon.theme') || key.startsWith('brand.theme')) {
-        delete config.entry[key];
-    }
-});
-
-// 2. Remove ParagonWebpackPlugin
-config.plugins = config.plugins.filter(
-    (plugin) => !(plugin instanceof ParagonWebpackPlugin),
-);
-
-// 3. Stub PARAGON_THEME global
-config.plugins.push(
-    new webpack.DefinePlugin({
-        PARAGON_THEME: JSON.stringify({}),
-    }),
-);
-
-// 4. Remove Paragon-specific cache groups
-if (config.optimization?.splitChunks?.cacheGroups) {
-    Object.keys(config.optimization.splitChunks.cacheGroups).forEach((key) => {
-        if (key.startsWith('paragon') || key.startsWith('brand')) {
-            delete config.optimization.splitChunks.cacheGroups[key];
-        }
-    });
-}
-
-// 5. Remove Paragon-specific CSS extraction rule
-if (config.module?.rules) {
-    config.module.rules = config.module.rules.map((rule) => {
-        if (rule.oneOf) {
-            return {
-                ...rule,
-                oneOf: rule.oneOf.filter((oneOfRule) => {
-                    const resourceStr = oneOfRule.resource?.toString() || '';
-                    return !resourceStr.includes('paragon') && !resourceStr.includes('brand');
-                }),
-            };
-        }
-        return rule;
-    });
-}
+config.module.rules[0].exclude = /node_modules\/(?!(fastest-levenshtein|@edx))/;
 
 module.exports = config;
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,59 +1,10 @@
 const { createConfig } = require('@openedx/frontend-build');
-const ParagonWebpackPlugin = require('@openedx/frontend-build/lib/plugins/paragon-webpack-plugin/ParagonWebpackPlugin');
-const webpack = require('webpack');
+const stripWutiskillParagonTheme = require('../webpack.wutiskill-theme');
 
 const config = createConfig('webpack-prod');
 
-// ---------------------------------------------------------------------------
-// Strip Paragon CSS from the production build.
-// See webpack.dev-tutor.config.js for full explanation.
-// ---------------------------------------------------------------------------
+stripWutiskillParagonTheme(config);
 
-// 1. Remove Paragon entrypoints
-Object.keys(config.entry).forEach((key) => {
-  if (key.startsWith('paragon.theme') || key.startsWith('brand.theme')) {
-    delete config.entry[key];
-  }
-});
-
-// 2. Remove ParagonWebpackPlugin
-config.plugins = config.plugins.filter(
-  (plugin) => !(plugin instanceof ParagonWebpackPlugin),
-);
-
-// 3. Stub PARAGON_THEME global
-config.plugins.push(
-  new webpack.DefinePlugin({
-    PARAGON_THEME: JSON.stringify({}),
-  }),
-);
-
-// 4. Remove Paragon-specific cache groups
-if (config.optimization?.splitChunks?.cacheGroups) {
-  Object.keys(config.optimization.splitChunks.cacheGroups).forEach((key) => {
-    if (key.startsWith('paragon') || key.startsWith('brand')) {
-      delete config.optimization.splitChunks.cacheGroups[key];
-    }
-  });
-}
-
-// 5. Remove Paragon-specific CSS extraction rule
-if (config.module?.rules) {
-  config.module.rules = config.module.rules.map((rule) => {
-    if (rule.oneOf) {
-      return {
-        ...rule,
-        oneOf: rule.oneOf.filter((oneOfRule) => {
-          const resourceStr = oneOfRule.resource?.toString() || '';
-          return !resourceStr.includes('paragon') && !resourceStr.includes('brand');
-        }),
-      };
-    }
-    return rule;
-  });
-}
-
-// 6. Keep the existing fastest-levenshtein exclusion
 config.module.rules[0].exclude = /node_modules\/(?!(fastest-levenshtein|@edx))/;
 
 module.exports = config;
